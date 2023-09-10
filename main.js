0820324608f6e1aef8d9b67177cb550c03b44b00
Alekosh <aakhosh00@gmail.com>
2023-09-10 18:33:48 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// const arrays = ['Тебе сегодня везет', 'Сегодня будет солнечно', 'Сегодня тебя будут ждать', 'В доте ты выиграешь'];
+const arrays = ['Тебе сегодня везет', 'Тебя понизят до Хлебера', 'Сегодня тебя повысят до Манагера', 'Завтра уходишь в псж'];
+
+const paper = document.querySelector('.paper p');
+document.querySelector('body').addEventListener('click', () => {
+  let item = arrays[Math.floor(Math.random() * arrays.length)];
+  paper.innerHTML = item;
+})
